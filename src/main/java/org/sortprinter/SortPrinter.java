0caeb92eefcd7573f62eceac1f1626d79bb31550
seongilman <seongilman@naver.com>
2024-03-13 11:18:38 +0900

    modify sort printer

--- src/main/java/org/sortprinter/SortPrinter.java
+++ src/main/java/org/sortprinter/SortPrinter.java
@@ -9,14 +9,17 @@ public class SortPrinter {
     private List<Integer> numbers = new ArrayList<>();
     private SortSolution solution = null;
 
-    void insertNumber(int num) {
+    //숫자 추가
+    public void insertNumber(int num) {
         this.numbers.add(num);
     }
-
-    void setSortSolution(SortSolution sortSolution) {
+    
+    //정렬 솔루션 선택
+    public void setSortSolution(SortSolution sortSolution) {
         this.solution = sortSolution;
     }
 
+    //선택된 정렬 방법으로 정렬
     public void sort() {
         if (this.solution == null) {
             System.out.println("sort solution is null. select sort solution.");
@@ -25,7 +28,13 @@ public class SortPrinter {
         this.solution.sort(numbers);
     }
 
+    //정렬된 결과 보기
     public void showNumbers() {
         this.numbers.forEach(System.out::println);
+    }
+
+    //숫자 초기화
+    public void clearNumbers() {
+        this.numbers = new ArrayList<>();
     }
 }
