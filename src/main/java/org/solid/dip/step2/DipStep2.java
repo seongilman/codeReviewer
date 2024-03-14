93edc041a8b0105278b30223b9afe13321e325e2
seongilman <seongilman@naver.com>
2024-03-14 10:31:12 +0900

    commit

--- src/main/java/org/solid/dip/step2/DipStep2.java
+++ src/main/java/org/solid/dip/step2/DipStep2.java
@@ -1,5 +1,8 @@
 package org.solid.dip.step2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 interface Notifier {
     String generateWeatherAlert(String weatherConditions);
 }
@@ -25,12 +28,23 @@ class WeatherTracker {
     Notifier notifier;
 
     public WeatherTracker(Notifier notifier) {
-        notifier = notifier;
+        this.notifier = notifier;
     }
 
     public void setCurrentConditions(String weatherDescription) {
         this.currentConditions = weatherDescription;
         String alert = notifier.generateWeatherAlert(weatherDescription);
         System.out.print(alert);
+    }
+}
+
+class WeatherTrackerTest {
+    public static void main(String[] args) {
+        Map<String, Notifier> notifierMap = new HashMap<>();
+        notifierMap.put("rainy", new Phone());
+        notifierMap.put("sunny", new Emailer());
+
+        WeatherTracker weatherTracker = new WeatherTracker(notifierMap.get("sunny"));
+//        WeatherTracker weatherTracker = new WeatherTracker(notifierMap.get("rainy"));
     }
 }
