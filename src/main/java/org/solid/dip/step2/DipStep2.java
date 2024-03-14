248c082751b8b027055be9a32fd80e86ad8f2f3b
seongilman <seongilman@naver.com>
2024-03-14 10:40:53 +0900

    commit

--- src/main/java/org/solid/dip/step2/DipStep2.java
+++ src/main/java/org/solid/dip/step2/DipStep2.java
@@ -24,7 +24,7 @@ class Phone implements Notifier {
 }
 
 class WeatherTracker {
-    private Map<String, Notifier> notifierMap = new HashMap<>();
+    private final Map<String, Notifier> notifierMap;
     private Notifier notifier;
 
     public WeatherTracker(Map<String, Notifier> notifierMap) {
@@ -40,10 +40,12 @@ class WeatherTracker {
 
 class WeatherTrackerTest {
     public static void main(String[] args) {
+        //Notifier 구현체 등록
         Map<String, Notifier> notifierMap = new HashMap<>();
         notifierMap.put("rainy", new Phone());
         notifierMap.put("sunny", new Emailer());
 
+        //Notifier 구현체 주입
         WeatherTracker weatherTracker = new WeatherTracker(notifierMap);
         weatherTracker.notify("rainy");
         weatherTracker.notify("sunny");
